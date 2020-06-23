c9eab5cdd6a6a886cfdf0ed5f265af27819f7492
CarlesRa <jcramos8282@gmail.com>
2020-06-23 13:10:24 +0200

    first commit

---
+++ src/app/recipes/recipes.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { Recipe } from './recipe.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipesService {
+
+  private recipes: Recipe[] = [
+    {
+      id: 'r1',
+      title: 'Schnitzel',
+      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+      ingredients: ['French Fries', 'Pork Meat', 'Salad']
+    },
+    {
+      id: 'r2',
+      title: 'Spaghetti',
+      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/b/b7/Lutong_Bahay_-_Bolognese_Spaghetti.jpg',
+      ingredients: ['Spaghetti', 'Meat', 'Tomatoes']
+    }
+  ];
+
+  constructor() { }
+
+  getAllRecipes() {
+    return [...this.recipes];
+  }
+
+  getRecipe(recipeId: string) {
+    return {...this.recipes.find(recipe => {
+      return recipe.id === recipeId;
+    })}
+  }
+}
